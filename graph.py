4e370b7ff8138d5605d400189f7acbf118b79828
Dorokhova Tatiana <t.dorohovav@gmail.com>
2017-04-27 16:15:42 +0500

    v2

--- graph.py
+++ graph.py
@@ -60,6 +60,7 @@ def get_queue(indexReduction, indexIncrease, node, stack):
                     stack.append(newSequence)
                     used.append(newSequence.name)
                     sequences.append(newSequence)
+                    findGraphs.update({newSequence.name: []})
 
                 newSequence = [value for value in sequences if value.name == name][0]
                 parent = { 'valueIncrease': copySequence[indexIncrease],
@@ -110,7 +111,10 @@ def generatingMaximumGraphs():
             
 
 def all_drawing_Sequence(graphs):
-    findGraphs = [graph['graph'] for graph in graphs]
+    for graph in graphs:
+        g = findGraphs[graph['sequence'].name]
+        g.append(graph['graph']);
+        findGraphs.update({graph['sequence'].name :g})
     count = 1
     while graphs:
         print("count ==", 1)
@@ -138,17 +142,59 @@ def all_drawing_Sequence(graphs):
                                 if sequence.length == indexReduction and neighbor != indexReduction:
                                     copygraph.add_edge(neighbor, indexReduction)
                                     copygraph.remove_edge(vertex, neighbor)
+                                    if True not in [nx.is_isomorphic(copygraph, g) for g in findGraphs[seq.name]]:
+                                        g = findGraphs[seq.name]
+                                        g.append(copygraph);
+                                        findGraphs.update({seq.name: g})
+                                        graphs.append({'graph': copygraph, 'sequence': seq})
+                                        for  h in findGraphs[seq.name]:
+                                            for ver1 in h:
+                                                rebra = h.adjacency_list()[ver1]
+                                                for k in rebra:
+                                                    for ver2 in h:
+                                                        if (ver2 != ver1 and h.degree(ver1) - h.degree(
+                                                                ver2) == 1 and k != ver2
+                                                                and k not in h.adjacency_list()[ver2]):
+                                                            copyGraph = copy.deepcopy(h)
+                                                            copyGraph.add_edge(k, ver2)
+                                                            copyGraph.remove_edge(k, ver1)
+                                                            if True not in [nx.is_isomorphic(copyGraph, g) for g in findGraphs[seq.name]]:
+                                                                g = findGraphs[seq.name]
+                                                                g.append(copyGraph)
+                                                                findGraphs.update({seq.name: g})
+                                                                graphs.append({'graph': copyGraph, 'sequence': seq})
+
 
                                 elif copygraph.degree(n) == sequence.sequence[indexReduction] and n not in copygraph.adjacency_list()[neighbor]:
 
                                     copygraph.add_edge(n, neighbor)
                                     copygraph.remove_edge(vertex, neighbor)
-                                if True not in [nx.is_isomorphic(copygraph, g) for g in findGraphs]:
-                                    graphs.append({'graph': copygraph, 'sequence': seq})
-                                    findGraphs.append(copygraph)
+                                    if True not in [nx.is_isomorphic(copygraph, g) for g in findGraphs[seq.name]]:
+                                        g = findGraphs[seq.name]
+                                        g.append(copygraph)
+                                        findGraphs.update({seq.name: g})
+
+                                        graphs.append({'graph': copygraph, 'sequence': seq})
+                                        for  h in findGraphs[seq.name]:
+                                            for ver1 in h:
+                                                rebra = h.adjacency_list()[ver1]
+                                                for k in rebra:
+                                                    for ver2 in h:
+                                                        if (ver2 != ver1 and h.degree(ver1) - h.degree(
+                                                                ver2) == 1 and k != ver2
+                                                                and k not in h.adjacency_list()[ver2]):
+                                                            copyGraph = copy.deepcopy(h)
+                                                            copyGraph.add_edge(k, ver2)
+                                                            copyGraph.remove_edge(k, ver1)
+                                                            if True not in [nx.is_isomorphic(copyGraph, g) for g in findGraphs[seq.name]]:
+                                                                g = findGraphs[seq.name]
+                                                                g.append(copyGraph)
+                                                                findGraphs.update({seq.name: g})
+                                                                graphs.append({'graph': copyGraph, 'sequence': seq})
+
 
 seq = Sequence([1] * 12)
-
+findGraphs = {}
 sequences.append(seq)
 bfs(seq)
 graphs = generatingMaximumGraphs()
